45afcb7bb2099467464c7113d670a10ad0c2c198
David Wahlstrom <david.wahlstrom@gmail.com>
2012-07-14 09:39:37 -0700

    be more careful with backups and update symlinks if links already exist

--- setup.sh
+++ setup.sh
@@ -1,17 +1,18 @@
 #this script will setup bash specifics using the configs in this repo
 
 files="bash bash_profile bash_login bash_aliases bashrc profile"
+date=`date +"%Y%m%d"`
 
 #for file in bash_profile bash_login bash_aliases bashrc profile; do
 for file in $files; do
     if [ -h ~/.$file ]; then
-        true
+        unlink ~/.$file
     else
-        if [ -e ~/.$file ]; then
-            echo "backing up ~/.$file to ~/.$file.bak"
-            mv ~/.$file ~/.$file.bak
+        if [ -f ~/.$file ]; then
+            echo "backing up ~/.$file to ~/.$file.bak$date"
+            mv ~/.$file ~/.$file.bak$date
         fi
-        echo "symlinking $PWD/$file to ~/.$file"
-        ln -s $PWD/$file ~/.$file
     fi
+    echo "symlinking $PWD/$file to ~/.$file"
+    ln -s $PWD/$file ~/.$file
 done
